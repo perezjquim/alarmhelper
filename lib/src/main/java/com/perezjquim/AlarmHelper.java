146d60f7f8da0f7162ed14dd84b6931614d20371
Joaquim Perez <jquim1@hotmail.com>
2018-03-22 13:13:11 +0000

    The beginning of them all

---
+++ lib/src/main/java/com/perezjquim/AlarmHelper.java
@@ -0,0 +1,29 @@
+package com.perezjquim;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+
+public abstract class AlarmHelper
+{
+    public static void setAlarm(Context context, String receiverName, long triggerTime, boolean repeating)
+    {
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(receiverName);
+        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,0);
+        if(!repeating)
+        {
+            alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() +
+                            triggerTime, pi);
+        }
+        else
+        {
+            alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + triggerTime,
+                    triggerTime, pi);
+        }
+    }
+}
