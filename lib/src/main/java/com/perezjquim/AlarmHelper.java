4f224e6b072e195c50595463f7b8d11b780bf10e
Joaquim Perez <jquim1@hotmail.com>
2018-06-07 18:02:34 +0100

    Cleanup

--- lib/src/main/java/com/perezjquim/AlarmHelper.java
+++ lib/src/main/java/com/perezjquim/AlarmHelper.java
@@ -8,12 +8,12 @@ import android.os.SystemClock;
 
 public abstract class AlarmHelper
 {
-    public static void scheduleAlarm(Context context, String receiverName, long triggerTime, boolean repeating)
+    public static void scheduleStandardAlarm(Context context, String receiverName, long triggerTime)
     {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(receiverName);
         PendingIntent pi = PendingIntent.getBroadcast(context,0,i,0);
-        if(!repeating)
+        if (alarmMgr != null)
         {
             alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                     SystemClock.elapsedRealtime() +
@@ -21,9 +21,24 @@ public abstract class AlarmHelper
         }
         else
         {
+            System.err.println("AlarmManager has a null pointer");
+        }
+    }
+
+    public static void scheduleRepeatingAlarm(Context context, String receiverName, long triggerTime)
+    {
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(receiverName);
+        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,0);
+        if (alarmMgr != null)
+        {
             alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                     SystemClock.elapsedRealtime() + triggerTime,
                     triggerTime, pi);
+        }
+        else
+        {
+            System.err.println("AlarmManager has a null pointer");
         }
     }
 }
