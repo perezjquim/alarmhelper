1cf3fd6177e3067b2b9516b5bae7402ab99fc12e
Joaquim Perez <jquim1@hotmail.com>
2018-08-28 14:23:42 +0100

    request codes can now be assigned to alarms

--- lib/src/main/java/com/perezjquim/AlarmHelper.java
+++ lib/src/main/java/com/perezjquim/AlarmHelper.java
@@ -9,28 +9,31 @@ import java.util.Calendar;
 
 public abstract class AlarmHelper
 {
-    public static void scheduleStandardAlarm(Context context, Class receiver, Calendar trigger)
+    public static void scheduleStandardAlarm(Context context, Class receiver, Calendar trigger, int requestCode)
     {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context,receiver);
-        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pi = PendingIntent.getBroadcast(context,requestCode,i,PendingIntent.FLAG_UPDATE_CURRENT);
         if (alarmMgr != null)
         {
             alarmMgr.cancel(pi);
             alarmMgr.set(AlarmManager.RTC_WAKEUP,
-                   trigger.getTimeInMillis(), pi);
+                    trigger.getTimeInMillis(), pi);
         }
         else
         {
             System.err.println("AlarmManager has a null pointer");
         }
     }
-
-    public static void scheduleStandardAlarm(Context context, Class receiver, long triggerTime)
+    public static void scheduleStandardAlarm(Context context, Class receiver, Calendar trigger)
+    {
+        scheduleStandardAlarm(context,receiver,trigger,0);
+    }
+    public static void scheduleStandardAlarm(Context context, Class receiver, long triggerTime, int requestCode)
     {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context,receiver);
-        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pi = PendingIntent.getBroadcast(context,requestCode,i,PendingIntent.FLAG_UPDATE_CURRENT);
         if (alarmMgr != null)
         {
             alarmMgr.cancel(pi);
@@ -43,12 +46,16 @@ public abstract class AlarmHelper
             System.err.println("AlarmManager has a null pointer");
         }
     }
+    public static void scheduleStandardAlarm(Context context, Class receiver, long triggerTime)
+    {
+        scheduleStandardAlarm(context,receiver,triggerTime,0);
+    }
 
-    public static void scheduleRepeatingAlarm(Context context, Class receiver, long interval)
+    public static void scheduleRepeatingAlarm(Context context, Class receiver, long interval, int requestCode)
     {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context,receiver);
-        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pi = PendingIntent.getBroadcast(context,requestCode,i,PendingIntent.FLAG_UPDATE_CURRENT);
         if (alarmMgr != null)
         {
             alarmMgr.cancel(pi);
@@ -61,12 +68,15 @@ public abstract class AlarmHelper
             System.err.println("AlarmManager has a null pointer");
         }
     }
-
-    public static void scheduleRepeatingAlarm(Context context, Class receiver, Calendar trigger, long interval)
+    public static void scheduleRepeatingAlarm(Context context, Class receiver, long interval)
+    {
+        scheduleRepeatingAlarm(context, receiver, interval ,0);
+    }
+    public static void scheduleRepeatingAlarm(Context context, Class receiver, Calendar trigger, long interval, int requestCode)
     {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context,receiver);
-        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pi = PendingIntent.getBroadcast(context,requestCode,i,PendingIntent.FLAG_UPDATE_CURRENT);
         if (alarmMgr != null)
         {
             alarmMgr.cancel(pi);
@@ -79,12 +89,16 @@ public abstract class AlarmHelper
             System.err.println("AlarmManager has a null pointer");
         }
     }
+    public static void scheduleRepeatingAlarm(Context context, Class receiver, Calendar trigger, long interval)
+    {
+        scheduleRepeatingAlarm(context, receiver, trigger, interval, 0);
+    }
 
-    public static void cancelAlarm(Context context, Class receiver)
+    public static void cancelAlarm(Context context, Class receiver, int requestCode)
     {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context,receiver);
-        PendingIntent pi = PendingIntent.getBroadcast(context,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pi = PendingIntent.getBroadcast(context,requestCode,i,PendingIntent.FLAG_UPDATE_CURRENT);
         if (alarmMgr != null)
         {
             alarmMgr.cancel(pi);
@@ -93,5 +107,9 @@ public abstract class AlarmHelper
         {
             System.err.println("AlarmManager has a null pointer");
         }
+    }
+    public static void cancelAlarm(Context context, Class receiver)
+    {
+        cancelAlarm(context, receiver, 0);
     }
 }
